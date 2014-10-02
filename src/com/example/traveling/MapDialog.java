eba7a18f715129b4048eb2bf255331f88153285f
lilyringing <lilytsai0918@gmail.com>
2014-10-02 14:43:52 +0800

    Modify MapDialog to dynamically show content
    http://www.cs.dartmouth.edu/~campbell/cs65/lecture13/lecture13.html

--- src/com/example/traveling/MapDialog.java
+++ src/com/example/traveling/MapDialog.java
@@ -7,12 +7,24 @@ import android.content.DialogInterface;
 import android.support.v4.app.DialogFragment;
 
 public class MapDialog extends DialogFragment {
+    public static MapDialog newInstance(String title, String list){
+        MapDialog mp = new MapDialog();
+        Bundle bun = new Bundle();
+        bun.putString("title", title);
+        bun.putString("list", list);
+        mp.setArguments(bun);
+        return mp;
+    }
+    
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String title = getArguments.getString("title");
+        String list = getArguments.getString("list");
+        
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.spot)
-        	   .setItems(R.array.spot_menu, new DialogInterface.OnClickListener(){
+        builder.setTitle(title)
+        	   .setItems(list, new DialogInterface.OnClickListener(){
         		   public void onClick(DialogInterface dialog, int which){
         			   
         		   }
@@ -20,8 +32,4 @@ public class MapDialog extends DialogFragment {
         // Create the AlertDialog object and return it
         return builder.create();
     }
-    
-    /*public static MapDialog newInstance(String title, String message){
-    	
-    }*/
 }
